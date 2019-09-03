0db70ef0dd242ce214837f400ab4a317cc0d4ab1
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-03 10:58:03 -0500

    files and folders structure created

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bcrypt = require('bcryptjs')
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
