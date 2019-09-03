87fc8ceb836d108d8249405da093de4ded32fd7b
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-03 13:05:08 -0500

    POST to api/register created, password hashed

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ const express = require('express');
 const server = express()
 const bcrypt = require('bcryptjs')
 
-const db = require('./database/db-config')
+
 const Users = require('./users/users-model')
 const restricted = require('./auth/restricted-middleware')
-
+server.use(express.json())
 
 server.get('/', (req, res) => {
   res.send('It\'s working')
 })
+
+
+// REGISTER - HASHING PASSWORD HERE
+server.post('/api/register', (req, res) => {
+  let user = req.body
+
+  // HASH PASSWORD
+  const hash = bcrypt.hashSync(user.password)
+  user.password = hash
+
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+})
+
 
 
 const port = process.env.PORT || 5000;
